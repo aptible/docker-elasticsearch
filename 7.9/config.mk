cb132a68f20a81d62ab042e478d26c9e31b54519
Alex Kubacki <usernotfound@gmail.com>
2020-11-13 10:23:24 -0700

    Bump version.

--- 7.9/config.mk
+++ 7.9/config.mk
@@ -1,3 +1,3 @@
-export ES_VERSION = 7.9.1
+export ES_VERSION = 7.9.3
 export DOWNLOAD_URL = https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$(ES_VERSION)-linux-x86_64.tar.gz
-export ES_SHA512SUM = e24aab0fbeb0b53cc386bb0ca1fc84c457851c5d80d147324bf97ff42f063332a93dec3c693550662393a72c7a0522a100181dd9a7d50b3e487a0f2a2a9bbcc0 
+export ES_SHA512SUM = bb02a5dc1caef97638a959ebba05dd649083c856334f30c670b851067292d7230e561d8759b15a80be73537d7a7efd9cef427d253cbb2efdbd6b168c6f9baa13 
