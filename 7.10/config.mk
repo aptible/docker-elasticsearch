b7cd396f9064a35508d45f3396ed8672b4cd753d
Alex Kubacki <alex@aptible.com>
2021-01-07 10:50:10 -0700

    Bump patch version. (#4)

--- 7.10/config.mk
+++ 7.10/config.mk
@@ -1,3 +1,3 @@
-export ES_VERSION = 7.10.0
+export ES_VERSION = 7.10.1
 export DOWNLOAD_URL = https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$(ES_VERSION)-linux-x86_64.tar.gz
-export ES_SHA512SUM = 5c159bdf0d6e140a2bee5fbb1c379fbe23b0ea39b01d715564f02e4674b444b065a8abfda86440229c4b70defa175722c479b60009b7eef7b3de66e2339aacea 
+export ES_SHA512SUM = 24c255dfbd7fc45d6b7e30b2adcd7ba7b44954bca6197eb8b6dddafafca2703f62a1f4ea260570c52f660ad3eb3f048d7cc2a958823f82d731398b21f54f8963 
